3fb4a72dff23405d5791638c36d9a37ee7b3609a
carpaudio <carpaudio@gmail.com>
2024-12-02 10:54:28 +0100

    New WIN Download Links 12-2-24

--- src/components/downloads.tsx
+++ src/components/downloads.tsx
@@ -8,8 +8,8 @@ const Downloads = () => {
       <div className='flex flex-col items-center  w-full'>
         <WindowsLogo className='h-[68px] text-white' />
         <p className='mt-5'>Windows / VST3</p>
-        <Link className='w-full mt-8 max-w-[450px] border border-white rounded-full py-4 text-center' target='_blank' href='https://dl.dropboxusercontent.com/scl/fi/4fod2i6glp2gcu5on88t9/All-CARP-Audio-Plugins-Win.zip?rlkey=hjzv3mhtrjcnln1uj912xw6z4&st=2ra3xgu9&dl=1'>Download Plugins</Link>
-        <p className='mt-4'>Prefer an Installer for Windows? <span><Link className='text-[#2A6C41]' target='_blank' href='https://dl.dropboxusercontent.com/scl/fi/d2cwnt2rng40c7ay33n2d/CARP-Audio-Plugin-Installer-Win.zip?rlkey=d2hd6goxfv9pz9iamq7yfogtk&st=siqahmh4&dl=1'>Download here</Link></span></p>
+        <Link className='w-full mt-8 max-w-[450px] border border-white rounded-full py-4 text-center' target='_blank' href='https://dl.dropboxusercontent.com/scl/fi/nj52dfu2g8s735tp57b2n/All-CARP-Audio-Plugins-Win.zip?rlkey=at2vc73rtt4fso5b6fxrs92ou&st=q6w4qppz&dl=1'>Download Plugins</Link>
+        <p className='mt-4'>Prefer an Installer for Windows? <span><Link className='text-[#2A6C41]' target='_blank' href='https://dl.dropboxusercontent.com/scl/fi/yz70gyvmv4jc8hbrwsvi9/CARP-Audio-Plugin-Installer-Win.zip?rlkey=qsnkntpo69sf8re7tj91pvuzo&st=0vzi0t6m&dl=1'>Download here</Link></span></p>
       </div>
       <div className='flex flex-col items-center  w-full'>
         <AppleLogo className='h-[65px] text-white'/>
