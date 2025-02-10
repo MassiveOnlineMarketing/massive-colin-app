0fd7c8e07acf8d270541edb9b9c4384ab18828b6
carpaudio <carpaudio@gmail.com>
2025-02-10 17:32:09 +0100

    Added Tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
 
 import { Toaster } from '@/components/toast/toaster'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -46,8 +47,35 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        id='google-tag-analytics-manager'
+        src={`https://www.googletagmanager.com/gtag/js?id=G-29DZW9T8BB`}
+        strategy="lazyOnload"
+      />
+      <Script id="google-tag-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', 'G-29DZW9T8BB', {
+                page_path: window.location.pathname,
+            });
+        `}
+      </Script>
+      <Script id='google-tag-manager' strategy="lazyOnload">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KFF97V7M');
+        `}
+      </Script>
+
       <body className={`${inter.className} overflow-x-hidden `}>
-        
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KFF97V7M"
+          height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe></noscript>
         {children}
 
         <Toaster />
